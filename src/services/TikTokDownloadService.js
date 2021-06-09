a969d2933d7f542cf34fefbf217b12827a1d04e5
Raul Jimenez <officialrauljimenez@gmail.com>
2021-06-08 19:38:18 -0500

    extended TikTokDownloadService class to accept www.tiktok.com/:userid/:videoid format

--- src/services/TikTokDownloadService.js
+++ src/services/TikTokDownloadService.js
@@ -5,22 +5,24 @@ module.exports = class TikTokDownloadService {
     constructor() {}
 
     async downloadVideo(expandedUrl, shortenedUrl) {
-        const id = this.parseUrlForId(expandedUrl);
-        let pathUrl = await this.useCLITiktokScraper(shortenedUrl, id);
-        console.log(pathUrl);
+        const id = (shortenedUrl) ? this.parseUrlForId(expandedUrl) : this.parseLongUrlForId(expandedUrl);
+        let pathUrl = await this.useCLITiktokScraper((shortenedUrl) ? shortenedUrl : expandedUrl, id);
         return pathUrl;
+    }
+
+    useCLITiktokScraper(url, id) {
+        return new Promise((resolve, reject) => {
+            exec(['tiktok-scraper', 'video', url, '-d'].join(' ')).on("close", (code, signal) => {
+                resolve(`${id}.mp4`)
+            })
+        })
     }
 
     parseUrlForId(url) {
         return url.split('/')[4].split('.')[0];
     }
 
-    useCLITiktokScraper(shortenedUrl, id) {
-        return new Promise((resolve, reject) => {
-            exec(['tiktok-scraper', 'video', shortenedUrl, '-d'].join(' ')).on("close", (code, signal) => {
-                resolve(`${id}.mp4`)
-            })
-        })
-    }
-
+    parseLongUrlForId(url) {
+        return url.split('/')[5].split('?')[0].split('/')[0];
+    }    
 }
