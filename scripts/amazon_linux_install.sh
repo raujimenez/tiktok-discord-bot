e5617d8c984a67b7b0e8ed913262e19928a552ab
Raul Jimenez <officialrauljimenez@gmail.com>
2022-01-20 14:57:20 -0600

    fix(scripts): adding missing deps

--- scripts/amazon_linux_install.sh
+++ scripts/amazon_linux_install.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# install needed dependencies 
+sudo apt-get install libgtk-3-0\
+          libcairo2\
+          libegl1\
+          libnotify4\
+          libgdk-pixbuf2.0-0\
+          libopus0\
+          libpango-1.0-0\
+          libharfbuzz0b\
+          libwoff1\
+          libharfbuzz-icu0\
+          gstreamer1.0-plugins-base\
+          libgstreamer-gl1.0-0\
+          gstreamer1.0-plugins-bad\
+          libjpeg-turbo8\
+          libopenjp2-7\
+          libwebpdemux2\
+          libwebp6\
+          libatk1.0-0\
+          libenchant1c2a\
+          libsecret-1-0\
+          libhyphen0\
+          libxdamage1\
+          libwayland-server0\
+          libwayland-egl1\
+          libxkbcommon0\
+          libepoxy0\
+          libevdev2\
+          libatk-bridge2.0-0\
+          libgles2\
+          gstreamer1.0-libav -y
+
 # download node
 sudo apt install nodejs -y
 sudo apt install npm -y
@@ -9,6 +41,7 @@ sudo apt-get install python3.6 python3-pip -y
 
 # install tiktokdownloader service
 pip3 install TikTokApi
+pip3 install requests
 python3 -m playwright install
 
 # install node dependencies
