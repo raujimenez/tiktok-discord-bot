3edf10b3d6e69f4ad6f64e3382e5ceb97b1be491
Raul Jimenez <officialrauljimenez@gmail.com>
2022-01-20 14:48:30 -0600

    fix(script): added yes tag

--- scripts/amazon_linux_install.sh
+++ scripts/amazon_linux_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # download node
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install nodejs -y
+sudo apt install npm -y
 
 # download python3 and pip3
-sudo apt-get update
-sudo apt-get install python3.6
+sudo apt-get install python3.6 -y
 
 # install tiktokdownloader service
 pip3 install TikTokApi
