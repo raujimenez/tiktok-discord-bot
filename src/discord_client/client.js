fb845b94db00db161d344bee613e561aba928e9e
Raul Jimenez <officialrauljimenez@gmail.com>
2022-01-20 20:29:25 -0600

    some helpful documentation and supress embed toggle

--- src/discord_client/client.js
+++ src/discord_client/client.js
@@ -8,6 +8,7 @@ const tiktokDownloadService = new TikTokDownloadService();
 
 const config = {
   deleteMessage: false,
+  suppressEmbeds: false,
 };
 
 client.on("ready", () => {
@@ -53,15 +54,27 @@ client.on("message", async (msg) => {
           if (config.deleteMessage) {
             msg.delete().catch((rejected) => console.log("BALLS: " + rejected));
           }
+          if (config.suppressEmbeds && !config.deleteMessage) {
+            msg.suppressEmbeds(config.suppressEmbeds);
+          }
         });
     } catch (error) {
       console.log(`failed with ${error}`);
     }
   } else if (msg.content == "!tictaco delete toggle") {
     config.deleteMessage = !config.deleteMessage;
-    msg.reply(`will now ${config.deleteMessage ? "" : "not"}delete message`);
+    msg.reply(`will now ${config.deleteMessage ? "" : "not "}delete message`);
+  } else if (msg.content == "!tictaco supress embed toggle") {
+    config.suppressEmbeds = !config.suppressEmbeds;
+    msg.reply(
+      `will now ${config.deleteMessage ? "" : "not "}supress embeds in messages`
+    );
   } else if (msg.author.id === BOT_ID) {
     msg.suppressEmbeds(true);
+  } else if (msg.content == "!tictaco help") {
+    msg.reply(
+      ">>> `!tictaco delete toggle`\nDeletes the original message you sent if enabled\n`!tictaco supress embed toggle`\nWill remove the Embed of your tiktok url to tidy things up"
+    );
   }
 });
 
