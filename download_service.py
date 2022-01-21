dcce4fa8513d002402e18a6c45d3a1dfe1f9a708
Raul Jimenez <officialrauljimenez@gmail.com>
2022-01-20 19:05:53 -0600

    added file closing to python file

--- download_service.py
+++ download_service.py
@@ -4,7 +4,8 @@ import sys
 def main(url, id):
   api = TikTokApi.get_instance()
   videoSchema = api.get_video_by_url(url)
-  open(f'{id}.mp4', 'wb').write(videoSchema)
-  
+  with open(f'{id}.mp4', 'wb') as f1:
+    f1.write(videoSchema)
+   
 
 main(sys.argv[1], sys.argv[2])
